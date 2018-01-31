bba4be19e570836ea1b400dad5043b66aa34d86e
Bruno Caú <brunoalcau@gmail.com>
2018-01-31 13:54:56 -0200

    Inicial project

---
+++ src/Components/shared/index.js
@@ -0,0 +1,6 @@
+export * from './InputText/InputText';
+export * from './Label/Label';
+export * from './Span/Span';
+export * from './Card/Card';
+export * from './Button/Button';
+export * from './List';
